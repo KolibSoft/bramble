9a16e6c0c0e5e7c578069a145523ccb0de5d7884
CHAVA <janitochvvll2311@gmail.com>
2023-07-03 16:42:26 -0600

    Get/Set context data

--- example/main.js
+++ example/main.js
@@ -1,4 +1,15 @@
 // import { } from "https://kolibsoft.github.io/bramble/lib/main.js";
-import { Path, Views } from "../lib/main.js";
+import { Data, Path, Views } from "../lib/main.js";
 
-console.log(Views.getViews());
+let views = Views.getViews();
+console.log(views);
+
+let data = {};
+document.body.data = data;
+
+console.log(Data.getData(views.item));
+console.log(Data.setData(views.item, "A value"));
+console.log(Data.getData(views.item));
+console.log(Data.setData(views.item, "Another value"));
+console.log(Data.getData(views.item));
+console.log(data);
