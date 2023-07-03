aa33c4110a27a39adf697cdea10ea8b916f9074a
CHAVA <janitochvvll2311@gmail.com>
2023-07-03 15:53:47 -0600

    Path class added

--- example/main.js
+++ example/main.js
@@ -1,2 +1,8 @@
 // import { } from "https://kolibsoft.github.io/bramble/lib/main.js";
-import { } from "../lib/main.js";
+import { Path } from "../lib/main.js";
+
+let object = {};
+Path.setValue(object, "A.B.C", "A value");
+console.log(Path.getValue(object, "A.B.C"));
+console.log(Path.getValue(object, "."));
+
