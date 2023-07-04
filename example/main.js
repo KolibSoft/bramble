e19aee796c080105b6210cddca3b8d33ce8ae229
CHAVA <janitochvvll2311@gmail.com>
2023-07-04 14:54:00 -0600

    Basic bindings

--- example/main.js
+++ example/main.js
@@ -1,14 +1,13 @@
 // import { } from "https://kolibsoft.github.io/bramble/lib/main.js";
-import { Data, Layouts, Path, Views } from "../lib/main.js";
+import { Binding, Data, Layouts, Path, Views } from "../lib/main.js";
+
+window.data = document.body.data = {
+    text: "My Text",
+    html: "<i>My html</i>",
+    value: "My value",
+    src: "https://th.bing.com/th/id/OIP.JoCzuQoEoDnMwAm9OuLbGQHaF0?pid=ImgDet&rs=1"
+};
 
 await Layouts.setLayouts();
+Binding.setBindings();
 let views = Views.getViews();
-console.log(views);
-console.log(views.item.layout);
-
-let data = {  };
-document.body.data = data;
-
-console.log(Data.getData(views.item));
-console.log(Data.setData(views.item, "A value"));
-console.log(data);
