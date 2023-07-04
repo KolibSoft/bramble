f9e029b9deaeeef3f846ffd8ad8f2748eb0dc29b
CHAVA <janitochvvll2311@gmail.com>
2023-07-04 15:09:22 -0600

    Basic javascript utils

--- example/main.js
+++ example/main.js
@@ -2,10 +2,12 @@
 import { Binding, Data, Layouts, Path, Views } from "../lib/main.js";
 
 window.data = document.body.data = {
-    text: "My Text",
-    html: "<i>My html</i>",
-    value: "My value",
-    src: "https://th.bing.com/th/id/OIP.JoCzuQoEoDnMwAm9OuLbGQHaF0?pid=ImgDet&rs=1"
+    "site-name": "My Example Site Name",
+    "main": {
+        "title": "My Main Content Title",
+        "content": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt enim adipisci quo debitis soluta quaerat reiciendis, quas, officia necessitatibus ipsa voluptatibus consectetur deleniti minus quia voluptatem voluptates dicta ad ullam!"
+    },
+    "form": {}
 };
 
 await Layouts.setLayouts();
