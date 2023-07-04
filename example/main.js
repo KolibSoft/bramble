61cf3338c77c9c54fd6b7b937a0dd7aaebd4f982
CHAVA <janitochvvll2311@gmail.com>
2023-07-04 16:22:16 -0600

    Live data example

--- example/main.js
+++ example/main.js
@@ -1,10 +1,10 @@
 // import { } from "https://kolibsoft.github.io/bramble/lib/main.js";
-import { Binding, Data, Layouts, Path, Views } from "../lib/main.js";
+import { Binding, Data, Layouts, LiveData, Path, Views } from "../lib/main.js";
 
 window.data = document.body.data = {
     "site-name": "My Example Site Name",
     "main": {
-        "title": "My Main Content Title",
+        "title": new LiveData("My Main Content Title"),
         "content": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt enim adipisci quo debitis soluta quaerat reiciendis, quas, officia necessitatibus ipsa voluptatibus consectetur deleniti minus quia voluptatem voluptates dicta ad ullam!"
     },
     "form": {}
@@ -13,3 +13,11 @@ window.data = document.body.data = {
 await Layouts.setLayouts();
 Binding.setBindings();
 let views = Views.getViews();
+
+let data = new LiveData();
+data.observe("owner", value => {
+    console.log(`Received value: ${value}`);
+});
+
+LiveData.runDispatcher();
+
